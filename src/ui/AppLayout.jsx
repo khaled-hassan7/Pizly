c362703e96fd9e5b49c75142e2a6d3e448296285
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-10 23:10:52 +0200

    style: update brand identity (logo name, fonts, colors) and global layout

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,22 +1,22 @@
 import { Outlet, useNavigation } from 'react-router-dom';
 import Header from './Header';
 import Loader from './Loader';
-import CartOverview from '../features/cart/CartOverview';
 
 function AppLayOut() {
   const navigation = useNavigation();
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {navigation.state === 'loading' && <Loader />}
-      <Header />
+
+      <div className="fixed left-0 top-0 z-50 w-full">
+        <Header />
+      </div>
 
       <div className="overflow-auto">
-        <main className="mx-auto max-w-3xl">
+        <main className="mx-auto">
           <Outlet />
         </main>
       </div>
-
-      <CartOverview />
     </div>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 
 function Button({ children, disabled, to, type, onClick }) {
   const base =
-    ' text-sm transtion-colors rounded-full bg-yellow-400  font-semibold text-stone-800 duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
+    ' text-sm transtion-colors rounded-full bg-pizly text-white font-semibold  duration-300 hover:bg-pizly-light focus:outline-none    disabled:cursor-not-allowed';
 
   const style = {
     primary: base + ' px-4 py-3  md:px-6 md:py-4',
     small: base + ' px-4 py-2  md:px-5 md:py-2.5 text-xs  ',
-    rounde: base + ' px-1.5 py-0.5  md:px-3 md:py-1.5 text-xs ',
+    rounde:
+      base + ' px-1.5 py-0.5  md:px-3 md:py-1.5 text-xs ',
     secondary:
       ' text-sm transtion-colors border-2 px-4 py-2.5  md:px-6 md:py-3.5  rounded-full  font-semibold text-stone-800 duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed',
   };
