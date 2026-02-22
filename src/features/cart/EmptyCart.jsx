e7a31c8c27e7545b4c81ad2a62304deb37b7e90a
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-22 02:21:20 +0200

    feat: add "Back to Menu" button to EmptyCart component

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,9 +1,25 @@
-function EmptyCart() {
+import Button from '../../ui/Button';
+function EmptyCart({ isFullPage = true, setIsOpen }) {
+  const containerStyle = isFullPage
+    ? 'flex flex-grow items-center justify-center pt-40 flex-col pb-8 '
+    : 'flex flex-grow items-center justify-center pt-20';
+
   return (
-    <div className="flex flex-grow items-center justify-center pt-20">
+    <div className={containerStyle}>
       <p className="text-sm font-medium text-stone-500">
         Add items to start your order. :)
       </p>
+      {isFullPage && (
+        <div className="mt-10">
+          <Button
+            type="primary"
+            to="/menu"
+            onClick={() => setIsOpen(false)}
+          >
+            Explore our Menu
+          </Button>
+        </div>
+      )}
     </div>
   );
 }
