886b50b06285a9746468bbe34f8a556a8a105c9d
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-24 03:25:43 +0200

    feat(header): implement route-based transparency and update logo to brand white

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,13 +4,26 @@ import MobileMinu from './MobileMinu';
 import CartOverview from '../features/cart/CartOverview';
 import { HeaderProvider } from '../context/HeaderContext';
 import MobileMenuToggle from './MobileMenuToggle';
+import { useLocation } from 'react-router-dom';
 
 function Header({ isScrolled }) {
+  const { pathname } = useLocation();
+
+  const isMenuPage = pathname === '/menu';
+
+  const headerBgClass = isMenuPage
+    ? isScrolled
+      ? 'bg-pizly'
+      : 'bg-pizly/40 '
+    : 'bg-pizly';
+
   return (
     <HeaderProvider isScrolled={isScrolled}>
       <header
-        className={`fixed left-0 right-0 z-50 mx-auto flex max-w-7xl items-center justify-between px-8 py-5 transition-all duration-700 ease-[cubic-bezier(0.33,1,0.68,1)] sm:mt-4 sm:w-[95%] sm:rounded-full sm:border-stone-300 sm:bg-pizly sm:shadow-lg ${
-          isScrolled ? 'bg-white/90 backdrop-blur-md' : ''
+        className={`fixed left-0 right-0 z-50 sm:bg-pizly mx-auto flex max-w-7xl items-center justify-between px-8 py-5 transition-all duration-700 ease-[cubic-bezier(0.33,1,0.68,1)] sm:w-[95%] sm:rounded-full sm:shadow-lg ${headerBgClass} ${
+          isScrolled
+            ? 'sm:mt-0 sm:border-b sm:border-white/20'
+            : 'sm:mt-4'
         } `}
       >
         {/* for mobile */}
--- src/ui/Logo.jsx
+++ src/ui/Logo.jsx
@@ -4,7 +4,7 @@ function Logo() {
   return (
     <Link
       to="/"
-      className="absolute left-1/2  -translate-x-1/2  text-3xl font-extrabold tracking-widest sm:static sm:left-auto sm:translate-x-0"
+      className="absolute left-1/2  -translate-x-1/2  text-3xl lg:text-4xl font-extrabold tracking-widest text-white sm:static sm:left-auto sm:translate-x-0"
     >
       Pizly
     </Link>
