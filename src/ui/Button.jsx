e60ee3e5cf3a3a9215f7831c87af187d26d6601d
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-24 02:00:03 +0200

    style(ui): refactor Button and LinkButton with brand colors and rounded variants

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -8,14 +8,19 @@ function Button({ children, disabled, to, type, onClick }) {
     primary: base + ' px-4 py-3  md:px-6 md:py-4',
     small: base + ' px-4 py-2  md:px-5 md:py-2.5 text-xs  ',
     rounde:
-      base + ' px-1.5 py-0.5  md:px-3 md:py-1.5 text-xs ',
+      base +
+      ' w-6 h-6 text-sm flex justify-center items-center ',
     secondary:
       ' text-sm transtion-colors border-2 px-4 py-2.5  md:px-6 md:py-3.5  rounded-full  font-semibold text-stone-800 duration-300 hover:bg-stone-300 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed',
   };
 
   if (to)
     return (
-      <Link className={style[type]} to={to}>
+      <Link
+        className={style[type]}
+        to={to}
+        onClick={onClick}
+      >
         {children}
       </Link>
     );
