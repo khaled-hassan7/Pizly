88d809238602edfab397fcd1604af46ed248d784
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-24 03:17:35 +0200

    feat(ui): implement body scroll lock in SlidePanel and optimize CreateOrder responsive layout

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -48,117 +48,122 @@ function CreateOrder() {
 
   if (!cart.length) return <EmptyCart isFullPage={true} />;
   return (
-    <div className="p-5 pt-40 max-w-2xl mx-auto w-full sm:shadow-md sm:bg-white rounded-xl  sm:pb-12 ">
-      <h2 className="mb-8 text-xl font-bold">
-        Ready to order? Let's go!
-      </h2>
-      <Form method="POST">
-        <div className="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-36">First Name</label>
-          <input
-            type="text"
-            name="customer"
-            required
-            className="input grow"
-            defaultValue={username}
-          />
-        </div>
-
-        <div className="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-36">
-            Phone number
-          </label>
-          <div className="grow">
-            <input
-              type="tel"
-              name="phone"
-              required
-              className="input w-full"
-            />
-            {errors?.phone && (
-              <p className="mt-2 w-fit rounded-md bg-red-100 p-1 text-sm">
-                {errors.phone}
-              </p>
-            )}
-          </div>
-        </div>
-
-        <div className="relative mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
-          <label className="sm:basis-36">Address</label>
-          <div className="grow">
+    <div className="min-h-[100dvh] pt-28 ">
+      <div className="mx-auto  max-w-2xl rounded-xl p-5 sm:bg-white sm:pb-12 sm:shadow-md">
+        <h2 className="mb-8 text-xl font-bold">
+          Ready to order? Let's go!
+        </h2>
+        <Form method="POST">
+          <div className="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <label className="sm:basis-36">First Name</label>
             <input
               type="text"
-              name="address"
+              name="customer"
               required
-              className="input w-full"
-              defaultValue={address}
-              disabled={isLoadingAddress}
+              className="input grow"
+              defaultValue={username}
             />
-            {addresStatus === 'error' && (
-              <p className="mt-2 w-fit rounded-md bg-red-100 p-1 text-sm">
-                {errorAdress}
-              </p>
+          </div>
+
+          <div className="mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <label className="sm:basis-36">
+              Phone number
+            </label>
+            <div className="grow">
+              <input
+                type="tel"
+                name="phone"
+                required
+                className="input w-full"
+              />
+              {errors?.phone && (
+                <p className="mt-2 w-fit rounded-md bg-red-100 p-1 text-sm">
+                  {errors.phone}
+                </p>
+              )}
+            </div>
+          </div>
+
+          <div className="relative mb-4 flex flex-col gap-2 sm:flex-row sm:items-center">
+            <label className="sm:basis-36">Address</label>
+            <div className="grow">
+              <input
+                type="text"
+                name="address"
+                required
+                className="input w-full"
+                defaultValue={address}
+                disabled={isLoadingAddress}
+              />
+              {addresStatus === 'error' && (
+                <p className="mt-2 w-fit rounded-md bg-red-100 p-1 text-sm">
+                  {errorAdress}
+                </p>
+              )}
+            </div>
+            {!position.longitude && !position.latitude && (
+              <span className="z-15 absolute right-1 top-10 sm:right-0.5 sm:top-1.5">
+                <Button
+                  disabled={isLoadingAddress}
+                  type="small"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    dispatch(fetchAddress());
+                  }}
+                >
+                  {' '}
+                  GET POSITION
+                </Button>
+              </span>
             )}
           </div>
-          {!position.longitude && !position.latitude && (
-            <span className="z-15 absolute right-1 top-10 sm:right-0.5 sm:top-1.5">
-              <Button
-                disabled={isLoadingAddress}
-                type="small"
-                onClick={(e) => {
-                  e.preventDefault();
-                  dispatch(fetchAddress());
-                }}
-              >
-                {' '}
-                GET POSITION
-              </Button>
-            </span>
-          )}
-        </div>
 
-        <div className="mb-12 flex items-center gap-2">
-          <input
-            className="h-4 w-4 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-1"
-            type="checkbox"
-            name="priority"
-            id="priority"
-            value={withPriority}
-            onChange={(e) =>
-              setWithPriority(e.target.checked)
-            }
-          />
-          <label htmlFor="priority" className="font-medium">
-            Want to yo give your order priority?
-          </label>
-        </div>
+          <div className="mb-12 flex items-center gap-2">
+            <input
+              className="h-4 w-4 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-1"
+              type="checkbox"
+              name="priority"
+              id="priority"
+              value={withPriority}
+              onChange={(e) =>
+                setWithPriority(e.target.checked)
+              }
+            />
+            <label
+              htmlFor="priority"
+              className="font-medium"
+            >
+              Want to yo give your order priority?
+            </label>
+          </div>
 
-        <div>
-          <input
-            type="hidden"
-            name="cart"
-            value={JSON.stringify(cart)}
-          />
-          <input
-            type="hidden"
-            name="position"
-            value={
-              position.longitude && position.latitude
-                ? `${position.latitude},${position.longitude}`
-                : ''
-            }
-          />
-          <Button
-            disabled={submitting || isLoadingAddress}
-            type="primary"
-          >
-            {' '}
-            {submitting
-              ? 'submitting..'
-              : ` Order now from ${totalPrice}`}{' '}
-          </Button>
-        </div>
-      </Form>
+          <div>
+            <input
+              type="hidden"
+              name="cart"
+              value={JSON.stringify(cart)}
+            />
+            <input
+              type="hidden"
+              name="position"
+              value={
+                position.longitude && position.latitude
+                  ? `${position.latitude},${position.longitude}`
+                  : ''
+              }
+            />
+            <Button
+              disabled={submitting || isLoadingAddress}
+              type="primary"
+            >
+              {' '}
+              {submitting
+                ? 'submitting..'
+                : ` Order now from ${totalPrice}`}{' '}
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
