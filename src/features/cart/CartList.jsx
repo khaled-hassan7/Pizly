e40164c98d53ec9333fdb26eb0671bed98836fa7
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-02-24 02:59:48 +0200

    refactor(cart): streamline CartList UI and optimize action buttons for mobile

--- src/features/cart/CartList.jsx
+++ src/features/cart/CartList.jsx
@@ -13,31 +13,28 @@ function CartList({ setIsOpen, cart }) {
 
   return (
     <div className="flex h-full flex-col">
-      <h2 className="border-b-2 py-5 text-center text-2xl font-extrabold">
-        Your order
-      </h2>
       <ul className="flex-1 divide-y divide-stone-200 overflow-y-auto border-b border-stone-200 pb-5 pl-6 pt-7">
         {cart.map((cart) => (
           <CartItem item={cart} key={cart.pizzaId} />
         ))}
       </ul>
-      <div className="mt-auto flex flex-shrink-0 items-center justify-between py-9">
-        <div className="space-x-2 px-2 sm:px-4">
+      <div className="mt-auto flex flex-shrink-0 items-center justify-between py-4">
+        <div className="flex space-x-2 px-2 sm:px-4">
           <Button
             onClick={() => setIsOpen(false)}
             type="primary"
             to="/order/new"
           >
-            Order pizzas
+            Order
           </Button>
           <Button
             type="secondary"
             onClick={handelClearCart}
           >
-            Clear cart
+            Clear
           </Button>
         </div>
-        <div className="flex items-center justify-center pr-2 sm:pr-4">
+        <div className="flex items-center pr-2 sm:pr-4">
           <p className="text-stone-600">
             total price{' '}
             <span className="text-center text-stone-900">
