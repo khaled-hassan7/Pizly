ca2b82f1570ad73b8df477b7f42638b53bec6c19
Khaled-Hassan7 <khaledhassan1907@gmail.com>
2026-04-25 20:33:15 +0300

    "fix: resolve hero layout shift and update image"

--- src/ui/Hero.jsx
+++ src/ui/Hero.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react';
 import Button from './Button';
 
 function Hero() {
+  useEffect(() => {
+    document.documentElement.classList.add(
+      'hide-scrollbar',
+    );
+
+    return () =>
+      document.documentElement.classList.remove(
+        'hide-scrollbar',
+      );
+  }, []);
+
   return (
-    <section className="relative h-[100dvh] touch-none  ">
+    <section className="relative w-screen overflow-hidden h-[100dvh]  touch-none">
       <div className="absolute inset-0 -z-10">
         <img
           src="../../src/assets/images/hero-img-2.jpg"
@@ -12,7 +24,7 @@ function Hero() {
         <div className="absolute inset-0 bg-black/40"></div>
       </div>
 
-      <div className="flex h-full flex-col items-center justify-center px-4 text-center ">
+      <div className="flex h-full flex-col items-center justify-center px-4 text-center">
         <div className="flex flex-col items-center">
           <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-6xl lg:text-7xl">
             The best pizza.
